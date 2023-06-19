9f1eacb2448717f4afc9c9c9410fe8c0ed816c5b
Adrian <acc@kladan.de>
2023-06-19 20:58:40 +0200

    Add and remove context menu entries

--- Program.cs
+++ Program.cs
@@ -1,11 +1,41 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
 
-bool _removeDate = args.Contains("-remove");
-bool _repairDate = args.Contains("-repair");
+bool _addDate = args.Contains("-addDate");
+bool _removeDate = args.Contains("-removeDate");
+bool _repairDate = args.Contains("-repairDate");
+bool _installTool = args.Contains("-install");
+bool _uninstallTool = args.Contains("-uninstall");
 
 string _sourceFilePath = args[0];
 
-RenameFile();
+if (_installTool)
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        AddContextMenuEntries();
+        Console.WriteLine("Zum Fortfahren Enter drücken.");
+        Console.ReadLine();
+    }
+}
+
+if (_uninstallTool)
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        RemoveFromContextMenu();
+        Console.WriteLine("Zum Fortfahren Enter drücken.");
+        Console.ReadLine();
+    }
+}
+
+if (_addDate || _removeDate || _repairDate)
+{
+    RenameFile();
+    Console.WriteLine("Zum Fortfahren Enter drücken.");
+    Console.ReadLine();
+}
 
 void RenameFile()
 {
@@ -18,6 +48,7 @@ void RenameFile()
             try
             {
                 File.Move(_sourceFilePath, destFilePath);
+                Console.WriteLine("Datei umbenannt.");
             }
             catch (Exception e)
             {
@@ -38,7 +69,8 @@ string ModifyFilename()
         destFilePath = Regex.Replace(_sourceFilePath, "\\d{4}\\-\\d{2}\\-\\d{2} ", "");
         destFilePath = Regex.Replace(destFilePath, "\\d{4}\\-\\d{2}\\-\\d{2}_", "");
     }
-    else if (_repairDate)
+    
+    if (_repairDate)
     {
         if (Regex.IsMatch(_sourceFilePath, "\\d{8}"))
         {
@@ -49,7 +81,8 @@ string ModifyFilename()
             destFilePath = Path.Combine(Path.GetDirectoryName(_sourceFilePath), destFileName);
         }
     }
-    else
+    
+    if (_addDate)
     {
         DateTime now = DateTime.Now;
         string date = now.ToString("yyyy-MM-dd");
@@ -58,4 +91,67 @@ string ModifyFilename()
     }
 
     return destFilePath;
+}
+
+void AddContextMenuEntries()
+{
+    try
+    {
+        AddContextMenuEntry("FNDateAdd", "Heutiges Datum vorne anfügen", "-addDate");
+        Console.WriteLine("\"Heutiges Datum vorne anfügen\" dem Kontextmenü hinzugefügt.");
+
+        AddContextMenuEntry("FNDateRemove", "Datum vorne entfernen", "-removeDate");
+        Console.WriteLine("\"Datum vorne entfernen\" dem Kontextmenü hinzugefügt.");
+
+        AddContextMenuEntry("FNDateRepair", "Datum vorne reparieren", "-repairDate");
+        Console.WriteLine("\"Datum vorne reparieren\" dem Kontextmenü hinzugefügt.");
+    }
+    catch (UnauthorizedAccessException e)
+    {
+        Console.WriteLine("Hierfür weden Administrator-Rechte benötigt.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+static void AddContextMenuEntry(string entryName, string entryText, string parameter)
+{
+    string exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+
+    RegistryKey root = Registry.ClassesRoot;
+    RegistryKey shell = root.OpenSubKey(@"*\shell", true);
+
+    RegistryKey filenameDateAdd = shell.CreateSubKey(entryName, true);
+    filenameDateAdd.SetValue("", entryText);
+
+    RegistryKey command = filenameDateAdd.CreateSubKey("command", true);
+    command.SetValue("", $"\"{exePath}\" \"%1\" {parameter}");
+}
+
+void RemoveFromContextMenu()
+{
+    try
+    {
+        RegistryKey root = Registry.ClassesRoot;
+        RegistryKey shell = root.OpenSubKey(@"*\shell", true);
+
+        shell.DeleteSubKeyTree("FNDateAdd", false);
+        Console.WriteLine("\"Heutiges Datum vorne anfügen\" aus dem Kontextmenü entfernt.");
+
+        shell.DeleteSubKeyTree("FNDateRemove", false);
+        Console.WriteLine("\"Datum vorne entfernen\" aus dem Kontextmenü entfernt.");
+
+        shell.DeleteSubKeyTree("FNDateRepair", false);
+        Console.WriteLine("\"Heutiges Datum vorne reparieren\" aus dem Kontextmenü entfernt.");
+    }
+    catch (UnauthorizedAccessException e)
+    {
+        Console.WriteLine("Hierfür weden Administrator-Rechte benötigt.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
 }
