c5c38d06c7512ca3bbee9c808f021ef39fb4dc4d
Adrian <acc@kladan.de>
2024-08-19 23:48:50 +0200

    Move again for parameterless install.exe and uninstall.exe

---
+++ install/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    AddContextMenuEntries();
+    Console.WriteLine("Zum Fortfahren Enter drücken.");
+    Console.ReadLine();
+}
+
+void AddContextMenuEntries()
+{
+    try
+    {
+        AddContextMenuEntry("FNDateAddToday", "Heutiges Datum vorne anfügen", "-addDateToday");
+        Console.WriteLine("\"Heutiges Datum vorne anfügen\" dem Kontextmenü hinzugefügt.");
+
+        AddContextMenuEntry("FNDateAddLastChange", "Änderungsdatum vorne anfügen", "-addDateLastChange");
+        Console.WriteLine("\"Änderungsdatum vorne anfügen\" dem Kontextmenü hinzugefügt.");
+
+        AddContextMenuEntry("FNDateRemove", "Datum vorne entfernen", "-removeDate");
+        Console.WriteLine("\"Datum vorne entfernen\" dem Kontextmenü hinzugefügt.");
+
+        AddContextMenuEntry("FNDateRepair", "Datum vorne reparieren", "-repairDate");
+        Console.WriteLine("\"Datum vorne reparieren\" dem Kontextmenü hinzugefügt.");
+    }
+    catch (UnauthorizedAccessException e)
+    {
+        Console.WriteLine("Hierfür weden Administrator-Rechte benötigt.");
+    }
+    catch (FileNotFoundException e)
+    {
+        Console.WriteLine($"{e.Message} {e.FileName}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+static void AddContextMenuEntry(string entryName, string entryText, string parameter)
+{
+    string exePathRelative = Path.Combine("RenameFile.exe");
+    string exePathAbsolute = Path.GetFullPath(exePathRelative);
+
+    if (!File.Exists(exePathAbsolute))
+    {
+        throw new FileNotFoundException("Die auszuführende Datei wurde nicht gefunden.", exePathAbsolute);
+    }
+
+    RegistryKey root = Registry.ClassesRoot;
+    RegistryKey shell = root.OpenSubKey(@"*\shell", true);
+
+    RegistryKey filenameDateAdd = shell.CreateSubKey(entryName, true);
+    filenameDateAdd.SetValue("", entryText);
+
+    RegistryKey command = filenameDateAdd.CreateSubKey("command", true);
+    command.SetValue("", $"\"{exePathAbsolute}\" \"%1\" {parameter}");
+}
---
+++ uninstall/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    RemoveFromContextMenu();
+    Console.WriteLine("Zum Fortfahren Enter drücken.");
+    Console.ReadLine();
+}
+
+void RemoveFromContextMenu()
+{
+    try
+    {
+        RegistryKey root = Registry.ClassesRoot;
+        RegistryKey shell = root.OpenSubKey(@"*\shell", true);
+
+        shell.DeleteSubKeyTree("FNDateAddToday", false);
+        Console.WriteLine("\"Heutiges Datum vorne anfügen\" aus dem Kontextmenü entfernt.");
+
+        shell.DeleteSubKeyTree("FNDateAddLastChange", false);
+        Console.WriteLine("\"Änderungsdatum vorne anfügen\" aus dem Kontextmenü entfernt.");
+
+        shell.DeleteSubKeyTree("FNDateRemove", false);
+        Console.WriteLine("\"Datum vorne entfernen\" aus dem Kontextmenü entfernt.");
+
+        shell.DeleteSubKeyTree("FNDateRepair", false);
+        Console.WriteLine("\"Heutiges Datum vorne reparieren\" aus dem Kontextmenü entfernt.");
+    }
+    catch (UnauthorizedAccessException e)
+    {
+        Console.WriteLine("Hierfür werden Administrator-Rechte benötigt.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
